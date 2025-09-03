b187a935a77694aa2f4cc18deabfe675ef54319f
khushichouhan2314 <114341359+khushichouhan2314@users.noreply.github.com>
2025-09-03 15:29:30 +0530

    Time: 0 ms (100%), Space: 41.8 MB (98.21%) - LeetHub

--- 2235-capitalize-the-title/2235-capitalize-the-title.java
+++ 2235-capitalize-the-title/2235-capitalize-the-title.java
@@ -1,22 +1,27 @@
 class Solution {
     public String capitalizeTitle(String title) {
-    
-        String[] words = title.toLowerCase().split(" ");
-        StringBuilder sb = new StringBuilder();
-        for(String word: words){
-            if(word.length()<3){
-                sb.append(word);
+        char arr[]= title.toCharArray();
+        int start = 0;
+        int n = arr.length;
+        for( int i = 0; i<=n;i++)
+        {
+            if(i ==n || arr[i]==' '){
+            int length = i-start;
+            if(length>=3)
+            {
+                if(arr[start]>='a'&& arr[start]<='z')
+                {
+                    arr[start] = (char)(arr[start]-32);
+                }
             }
-            else{
-                sb.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1));
-            }
-            sb.append(" ");
+            start=i+1;
         }
-       
-       
-  
-return sb.toString().trim();
-        
-        
+         else{
+            if(arr[i]>='A' && arr[i]<='Z'){
+                arr[i]= (char)(arr[i]+32);
+            }
+        }
     }
+    return new String(arr);
+}
 }
